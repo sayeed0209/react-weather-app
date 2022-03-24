56a7507cd5549e7ca8cb2394f89fb10004e97d42
sayeed <sayeed0209@gmail.com>
2022-03-24 17:34:27 +0100

    Initial project setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import Search from './components/Search';
 import './App.css';
 
+// API_KEY 5ec058da87e5012975ef3a71ff6c9603
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			
+			<Search />
+		
+		</div>
+	);
 }
 
 export default App;
